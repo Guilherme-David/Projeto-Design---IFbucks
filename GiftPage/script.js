5cb4b225465f3ff0fa96a10f35a34ab63d1181c3
Fabian <160931865+FabianMessy@users.noreply.github.com>
2025-02-28 20:24:44 -0300

    Add files via upload

---
+++ GiftPage/script.js
@@ -0,0 +1,127 @@
+var btn1 = document.querySelector('#q1')
+var container1 = document.querySelector('.q1-c')
+
+btn1.addEventListener('click', function() {
+
+    if(container1.style.display === 'block') {
+        container1.style.display = 'none'
+        btn1.textContent = "+"
+    }
+
+    else {
+        container1.style.display = 'block'
+        btn1.textContent = "-"
+    }
+});
+
+var btn2 = document.querySelector('#q2')
+var container2 = document.querySelector('.q2-c')
+
+btn2.addEventListener('click', function() {
+
+    if(container2.style.display === 'block') {
+        container2.style.display = 'none'
+        btn2.textContent = "+"
+    }
+
+    else {
+        container2.style.display = 'block'
+        btn2.textContent = "-"
+    }
+});
+
+var btn3 = document.querySelector('#q3')
+var container3 = document.querySelector('.q3-c')
+
+btn3.addEventListener('click', function() {
+
+    if(container3.style.display === 'block') {
+        container3.style.display = 'none'
+        btn3.textContent = "+"
+    }
+
+    else {
+        container3.style.display = 'block'
+        btn3.textContent = "-"
+    }
+});
+
+var btn4 = document.querySelector('#q4')
+var container4 = document.querySelector('.q4-c')
+
+btn4.addEventListener('click', function() {
+
+    if(container4.style.display === 'block') {
+        container4.style.display = 'none'
+        btn4.textContent = "+"
+    }
+
+    else {
+        container4.style.display = 'block'
+        btn4.textContent = "-"
+    }
+});
+
+var btn5 = document.querySelector('#q5')
+var container5 = document.querySelector('.q5-c')
+
+btn5.addEventListener('click', function() {
+
+    if(container5.style.display === 'block') {
+        container5.style.display = 'none'
+        btn5.textContent = "+"
+    }
+
+    else {
+        container5.style.display = 'block'
+        btn5.textContent = "-"
+    }
+});
+
+var btn6 = document.querySelector('#q6')
+var container6 = document.querySelector('.q6-c')
+
+btn6.addEventListener('click', function() {
+
+    if(container6.style.display === 'block') {
+        container6.style.display = 'none'
+        btn6.textContent = "+"
+    }
+
+    else {
+        container6.style.display = 'block'
+        btn6.textContent = "-"
+    }
+});
+
+var btn7 = document.querySelector('#q7')
+var container7 = document.querySelector('.q7-c')
+
+btn7.addEventListener('click', function() {
+
+    if(container7.style.display === 'block') {
+        container7.style.display = 'none'
+        btn7.textContent = "+"
+    }
+
+    else {
+        container7.style.display = 'block'
+        btn7.textContent = "-"
+    }
+});
+
+var btn8 = document.querySelector('#q8')
+var container8 = document.querySelector('.q8-c')
+
+btn8.addEventListener('click', function() {
+
+    if(container8.style.display === 'block') {
+        container8.style.display = 'none'
+        btn8.textContent = "+"
+    }
+
+    else {
+        container8.style.display = 'block'
+        btn8.textContent = "-"
+    }
+});
